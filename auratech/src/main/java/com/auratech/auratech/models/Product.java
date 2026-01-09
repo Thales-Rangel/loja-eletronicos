7cbd2130b1c302f3ff16450523f0cab469cb8944
Thales-Rangel <thalesran2006@gmail.com>
2026-01-09 20:51:54 -0300

    add model product

---
+++ auratech/src/main/java/com/auratech/auratech/models/Product.java
@@ -0,0 +1,79 @@
+package com.auratech.auratech.models;
+
+import org.springframework.beans.BeanUtils;
+
+import com.auratech.auratech.dto.ProductDTO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String brand;
+	private String description;
+	private int quantity;
+	private float price;
+
+	public Product(ProductDTO dto) {
+		BeanUtils.copyProperties(dto, this);
+	}
+
+	public Product() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
