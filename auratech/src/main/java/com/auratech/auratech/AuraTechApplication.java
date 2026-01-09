857a0b00acd223c7896bd1cb2b8e833dbfb291ba
Thales-Rangel <thalesran2006@gmail.com>
2026-01-09 13:43:57 -0300

    add backend auratech

---
+++ auratech/src/main/java/com/auratech/auratech/AuraTechApplication.java
@@ -0,0 +1,13 @@
+package com.auratech.auratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraTechApplication.class, args);
+	}
+
+}
---
+++ auratech/src/test/java/com/auratech/auratech/AuraTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auratech.auratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
