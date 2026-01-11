c95cd270761c69616e4a64f67d1c3f8fae75bcff
Thales-Rangel <thalesran2006@gmail.com>
2026-01-10 22:23:27 -0300

    add repository annotation

--- auratech/src/main/java/com/auratech/auratech/repositories/ShoppingListRepository.java
+++ auratech/src/main/java/com/auratech/auratech/repositories/ShoppingListRepository.java
@@ -3,10 +3,12 @@ package com.auratech.auratech.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.auratech.auratech.models.Client;
 import com.auratech.auratech.models.ShoppingList;
 
+@Repository
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
 	
 	List<ShoppingList> findByClient(Client client);
