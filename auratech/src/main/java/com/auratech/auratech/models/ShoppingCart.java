7ec228cf3fc9a6926be60b931d9b3ca79fcf4745
Thales-Rangel <thalesran2006@gmail.com>
2026-01-11 00:04:30 -0300

    add model shopping cart

---
+++ auratech/src/main/java/com/auratech/auratech/models/ShoppingCart.java
@@ -0,0 +1,48 @@
+package com.auratech.auratech.models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class ShoppingCart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	private Client client;
+	@ManyToMany
+	private List<Product> products;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+}
