e504b9eda97cb30a51935a55ee89775a3d78308f
Thales-Rangel <thalesran2006@gmail.com>
2026-01-12 14:56:30 -0300

    add model purchase order

---
+++ auratech/src/main/java/com/auratech/auratech/models/PurchaseOrder.java
@@ -0,0 +1,57 @@
+package com.auratech.auratech.models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PurchaseOrder {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private float totalPrice;
+
+	@ManyToOne
+	private Client client;
+	@ManyToMany
+	private List<Product> products;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public float getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(float totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+}
